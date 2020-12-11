a3c9dc286ba80d897b03ded66d0a150d215cf808
moussa Abu Walid <moussa79@gmail.com>
2020-12-11 04:10:11 +0000

    Add project files.

---
+++ ClassLibrary1/Entity/EntityBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CORE.Entity
+{
+    public class EntityBase
+    {
+        public Guid Id { get; set; }
+    }
+   public  class Owner : EntityBase
+    {
+        public string Fullname { get; set; }
+        public string Profil { get; set; }
+
+        public string Avatar { get; set; }
+        public Address Address { get; set; }
+    }
+    public class Address : EntityBase
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int Number { get; set; }
+    }
+    public class Portofolio: EntityBase
+    {
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+    }
+
+}
